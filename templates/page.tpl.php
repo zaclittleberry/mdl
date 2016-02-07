800f90a27b208440f18cc92800a92f678efb78f3
ZacDavidM <zacdavidm@gmail.com>
2016-02-07 15:47:26 -0500

    add in page content region

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -29,6 +29,8 @@
       <!--/.l-messages -->
     <?php endif; ?>
 
+    <?php print render($page['content']); ?>
+
     <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
       <header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
         <i class="material-icons">play_circle_filled</i>
